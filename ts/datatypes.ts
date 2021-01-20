1a31c71ed66446ed1c20bbb3596ea038a97d6af9
srikanthpragada <srikanthpragada@gmail.com>
2021-01-20 16:55:33 +0530

    20-JAN

---
+++ ts/datatypes.ts
@@ -0,0 +1,10 @@
+var uname : string;
+var price : number  = 100;
+var email : string;
+
+//  uname = "Abc"
+price = 200
+
+console.log(uname)
+console.log(price)
+console.log(email)
---
+++ ts/strings.ts
@@ -0,0 +1,6 @@
+
+var uname : string = "Srikanth";
+var subject : string = "Angular";
+var sentence = `Hello, I am ${uname} and I am your ${subject} trainer!`
+
+console.log(sentence)
